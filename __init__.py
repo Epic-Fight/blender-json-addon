97ae6b136e946a1f7752541a481ceddbadd8777b
Guilherme <77132244+guivnf@users.noreply.github.com>
2026-02-16 06:04:08 -0300

    Fixed miss commit

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 bl_info = {
     "name": "Minecraft Model Json Exporter",
-    "author": "Yesssssman",
-    "blender": (2, 80, 0),
+    "author": "Yesssssman, box, Guivnf",
+    "blender": (3, 6, 0),
     "category": "Import-Export",
     "location": "File > Import-Export",
-    "description": "Specially designed exporter for developing Minecraft Epic Fight Mod"
+    "description": "Epic Fight JSON Exporter - Blender 2.8+"
 }
 
 import bpy
-from bpy.props import *
+from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy_extras.io_utils import ExportHelper
 
 
@@ -17,71 +17,68 @@ class ExportToJson(bpy.types.Operator, ExportHelper):
     bl_idname = "export_mc.json"
     bl_label = "Export to Json for Minecraft"
     filename_ext = ".json"
-    filter_glob = StringProperty(default="*.json", options={"HIDDEN"})
+    filter_glob: StringProperty(default="*.json", options={"HIDDEN"})
 
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
 
-    # export toggles (ordered top to bottom in the panel)
-
-    export_mesh = BoolProperty(
+    export_mesh: BoolProperty(
         name="Export Mesh",
         description="Export mesh data",
         default=True
     )
 
-    apply_modifiers = BoolProperty(
+    apply_modifiers: BoolProperty(
         name="Apply Modifiers",
         description="Apply modifiers before exporting. "
                     "Make sure the armature is in rest pose if enabled",
         default=False
     )
 
-    export_armature = BoolProperty(
+    export_armature: BoolProperty(
         name="Export Armature",
         description="Export armature data",
         default=True
     )
 
-    transform_formats = [
-        ('MAT', 'Matrix', "Export transform as matrix"),
-        ('ATTR', 'Attributes', "Export transform as loc, rot, scale attributes")
-    ]
-
-    armature_format = EnumProperty(
+    armature_format: EnumProperty(
         name="Armature Format",
         description="Armature transform export format",
-        default='ATTR',
-        items=transform_formats
+        default='MAT',
+        items=[
+            ('MAT', 'Matrix', "Export transform as matrix"),
+            ('ATTR', 'Attributes', "Export transform as loc, rot, scale attributes")
+        ]
     )
 
-    export_anim = BoolProperty(
+    export_anim: BoolProperty(
         name="Export Animation",
         description="Export animation data",
         default=True
     )
 
-    animation_format = EnumProperty(
+    animation_format: EnumProperty(
         name="Animation Format",
         description="Animation transform export format",
         default='ATTR',
-        items=transform_formats
+        items=[
+            ('MAT', 'Matrix', "Export transform as matrix"),
+            ('ATTR', 'Attributes', "Export transform as loc, rot, scale attributes")
+        ]
     )
 
-    export_camera = BoolProperty(
+    export_camera: BoolProperty(
         name="Export Camera",
         description="Export camera transform (always exported as Attributes)",
         default=False
     )
 
-    export_only_visible_bones = BoolProperty(
+    export_only_visible_bones: BoolProperty(
         name="Export Only Visible Bones",
         description="Export bones that are visible. Warning: child bones of hidden bones won't export either",
         default=False
     )
-
-    # custom draw so controls appear in the right order
 
     def draw(self, context):
         layout = self.layout
@@ -107,11 +104,10 @@ class ExportToJson(bpy.types.Operator, ExportHelper):
         layout.prop(self, "export_camera")
         if self.export_camera:
             box = layout.box()
-            box.label("Camera is always exported as Attributes", icon='INFO')
+            box.label(text="Camera is always exported as Attributes", icon='INFO')
 
         layout.separator()
         layout.prop(self, "export_only_visible_bones")
-
 
     def execute(self, context):
         if not self.filepath:
@@ -129,13 +125,13 @@ def menu_func(self, context):
 
 
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_export.append(menu_func)
+    bpy.utils.register_class(ExportToJson)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func)
 
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_file_export.remove(menu_func)
+    bpy.utils.unregister_class(ExportToJson)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func)
 
 
 if __name__ == "__main__":
